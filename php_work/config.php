191a9f32b4a98db0940204b63af0e8488f0cdbb2
piusha <piushakalyana@gmail.com>
2018-06-02 10:02:31 +0530

    Clean Dir

--- php_work/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DB_HOST','db.iplaydeal.loc');
-define('DB_USER_NAME','root');
-define('DB_PASSWORD','rootpass');
-define ('DB_NAME','zeta');
-
-define('MODEL_PATH','App/Model');
-define('ENTITY_PATH','App/Entity');
