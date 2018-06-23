1a15170ef4603ed2825cdb8798261aa68bbd9c4f
piusha <piushakalyana@gmail.com>
2018-06-23 11:08:20 +0530

    Header Navigation

--- admin_panel/resources/views/auth/register.blade.php
+++ admin_panel/resources/views/auth/register.blade.php
@@ -1,77 +1,31 @@
-@extends('layouts.app')
+@extends('layouts.admin_login')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+	<div class="page-content container">
+		<div class="row">
+			<div class="col-md-4 col-md-offset-4">
+				<div class="login-wrapper">
+			        <div class="box">
+			            <div class="content-wrap">
+			                <h6>Sign Up</h6>
+			                <input class="form-control" type="text" placeholder="E-mail address">
+			                <input class="form-control" type="password" placeholder="Password">
+			                <input class="form-control" type="password" placeholder="Confirm Password">
+			                <div class="action">
+			                    <a class="btn btn-primary signup" href="index.html">Sign Up</a>
+			                </div>                
+			            </div>
+			        </div>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+			        <div class="already">
+			            <p>Have an account already?</p>
+			            <a href="login.html">Login</a>
+			        </div>
+			    </div>
+			</div>
+		</div>
+	</div>
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
--- admin_panel/resources/views/includes/header.blade.php
+++ admin_panel/resources/views/includes/header.blade.php
@@ -1,11 +1,47 @@
-
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
-            <!-- Logo -->
-            <div class="logo">
-                <h1><a href="index.html">Bootstrap Admin Theme</a></h1>
+        @guest
+            <div class="col-md-12">
+                <!-- Logo -->
+                <div class="logo">
+                    <h1><a href="index.html">Bootstrap Admin Theme</a></h1>
+                </div>
             </div>
-        </div>
+        @else
+            <div class="col-md-5">
+                <!-- Logo -->
+                <div class="logo">
+                    <h1><a href="index.html">Bootstrap Admin Theme</a></h1>
+                </div>
+            </div>
+            
+            <div class="col-md-7">
+                <div class="navbar navbar-inverse" role="banner">
+                    
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                                        {{ Auth::user()->name }} <span class="caret"></span>
+                                    </a>
+
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a href="{{ route('logout') }}"
+                                                onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                                Logout
+                                            </a>
+
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                            </form>
+                                        </li>
+                                    </ul>
+                                </li>
+                        </ul>
+                    
+                </div>
+            </div>
+        @endguest
     </div>
 </div>
